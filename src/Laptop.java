16eb03a73ca58a538b1efa648249387ab42bf62e
Hdbygdla <131492426+MiguelMoran1234@users.noreply.github.com>
2024-11-08 13:42:21 -0600

    Se agregaron clases PC, de_Escritorio y Laptop

---
+++ src/Laptop.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author flash
+ */
+public class Laptop extends PC{
+    
+    private String marca;
+    private String definicion;
+    private boolean rgb;
+
+    public Laptop(String marca, String definicion, boolean rgb) {
+        this.marca = marca;
+        this.definicion = definicion;
+        this.rgb = rgb;
+    }
+    
+}
---
+++ src/de_Escritorio.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author flash
+ */
+public class de_Escritorio extends PC{
+    
+    private int ram;
+    private int almacenamiento;
+    private String tipo_almacenamiento;
+    private boolean grafica;
+
+    public de_Escritorio(int ram, int almacenamiento, String tipo_almacenamiento, boolean grafica) {
+        this.ram = ram;
+        this.almacenamiento = almacenamiento;
+        this.tipo_almacenamiento = tipo_almacenamiento;
+        this.grafica = grafica;
+    }
+    
+    
+    
+}
